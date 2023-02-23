75bee797dd93c6f2cd5cd2ba3e6378d60f07eb4e
Darren Ong <Darren.Ong@WhiteCoat.Global>
2023-02-23 19:36:18 +0800

    feat: #1 init commit

---
+++ mxp01/src/main/java/com/example/mxp01/Mxp01Application.java
@@ -0,0 +1,13 @@
+package com.example.mxp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mxp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mxp01Application.class, args);
+	}
+
+}
---
+++ mxp01/src/test/java/com/example/mxp01/Mxp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mxp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mxp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
