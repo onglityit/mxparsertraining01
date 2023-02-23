04331771b2180651af761960e8c34d23a9368b51
Darren Ong <Darren.Ong@WhiteCoat.Global>
2023-02-23 20:44:04 +0800

    feat: #1 print help

--- mxp01/src/main/java/com/example/mxp01/Mxp01Application.java
+++ mxp01/src/main/java/com/example/mxp01/Mxp01Application.java
@@ -2,12 +2,15 @@ package com.example.mxp01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mariuszgromada.math.mxparser.mXparser;
 
 @SpringBootApplication
 public class Mxp01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mxp01Application.class, args);
+		//SpringApplication.run(Mxp01Application.class, args);
+		mXparser.consolePrintHelp("sine");
+		mXparser.consolePrintHelp("key=sin");
 	}
 
 }
